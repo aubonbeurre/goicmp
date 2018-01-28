f602f60018d2d68ac91e21ff53755f5ce81a0bd8
Alexandre Parenteau <aubonbeurre@gmail.com>
2018-01-28 18:36:42 +0100

    test

--- goicmp.go
+++ goicmp.go
@@ -7,7 +7,10 @@ import (
 	"os"
 
 	"github.com/aubonbeurre/goicmp/utils"
+	gostats "github.com/aubonbeurre/gostats/utils"
+	"github.com/bndr/gojenkins"
 	"github.com/jessevdk/go-flags"
+	"github.com/olekukonko/tablewriter"
 )
 
 var (
@@ -23,6 +26,37 @@ var gOpts struct {
 	Verbose []bool `short:"v" long:"verbose" description:"Show verbose debug information"`
 	Batch   string `short:"b" long:"batch" description:"A json file with a list of diff" value-name:"BATCHFILE"`
 	Out     string `short:"o" long:"output" description:"A json output with the results" value-name:"OUTPUT"`
+	Jenkins bool   `short:"j" long:"jenkins" description:"Jenkins mode"`
+}
+
+func goJenkins() (err error) {
+	gostats.GPrefs.Load()
+	var jenkins = gostats.NewJenkinsClient()
+	var jobs []gojenkins.InnerJob
+	if jobs, err = jenkins.GetAllJobNames(); err != nil {
+		return err
+	}
+	var data = make([][]string, 0)
+	var idx = 1
+	for _, job := range jobs {
+		var row = make([]string, 0)
+		if job.Color == "notbuilt" || job.Color == "disabled" {
+			continue
+		}
+		row = append(row, fmt.Sprintf("%d", idx))
+		row = append(row, job.Name)
+		row = append(row, job.Color)
+		row = append(row, job.Url)
+		data = append(data, row)
+		idx++
+	}
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Index", "Name", "Color", "Url"})
+	for _, v := range data {
+		table.Append(v)
+	}
+	table.Render() // Send output
+	return nil
 }
 
 func main() {
@@ -32,6 +66,13 @@ func main() {
 	var args []string
 	if args, err = parser.Parse(); err != nil {
 		os.Exit(1)
+	}
+
+	if gOpts.Jenkins {
+		if err = goJenkins(); err != nil {
+			panic(err)
+		}
+		os.Exit(0)
 	}
 
 	if len(gOpts.Batch) > 0 {
